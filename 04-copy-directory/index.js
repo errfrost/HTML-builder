b7532d74b0b37836b8f04f8f55c1849bc33b52b0
Vladimir Kurganov <err0rfrost@gmail.com>
2022-11-08 12:01:57 +0600

    feat: copy dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const { readdir } = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
 
+const dirIn = path.join(__dirname, 'files');
+const dirOut = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  try {
+    fs.rmSync(dirOut, { force: true, recursive: true });
+    fs.mkdirSync(dirOut, { recursive: true });
+
+    const files = await readdir(dirIn);
+    for (const file of files) {
+      let input = fs.createReadStream(path.join(dirIn, file));
+      let output = fs.createWriteStream(path.join(dirOut, file));      
+      input.pipe(output);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyDir();
